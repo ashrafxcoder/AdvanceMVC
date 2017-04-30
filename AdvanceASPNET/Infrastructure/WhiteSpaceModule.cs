ecfbd8b6054517d4ba5538a9dc7e4f0b1b8c7677
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-05-01 00:41:41 +0500

    Commented out

--- AdvanceASPNET/Infrastructure/WhiteSpaceModule.cs
+++ AdvanceASPNET/Infrastructure/WhiteSpaceModule.cs
@@ -1,137 +1,137 @@
-﻿using System.IO;
-using System.Text;
-using System.Web;
-using WebMarkupMin;
-using WebMarkupMin.Core;
+﻿//using System.IO;
+//using System.Text;
+//using System.Web;
+//using WebMarkupMin;
+//using WebMarkupMin.Core;
 
-namespace AdvanceASPNET.Infrastructure
-{
-    public class WhitespaceModule : IHttpModule
-    {
-        #region IHttpModule Members
+//namespace AdvanceASPNET.Infrastructure
+//{
+//    public class WhitespaceModule : IHttpModule
+//    {
+//        #region IHttpModule Members
 
-        void IHttpModule.Dispose()
-        {
-            // Nothing to dispose;
-        }
+//        void IHttpModule.Dispose()
+//        {
+//            // Nothing to dispose;
+//        }
 
-        void IHttpModule.Init(HttpApplication app)
-        {
-            app.PostRequestHandlerExecute += (o, e) => PostRequestHandlerExecute(app);
-        }
+//        void IHttpModule.Init(HttpApplication app)
+//        {
+//            app.PostRequestHandlerExecute += (o, e) => PostRequestHandlerExecute(app);
+//        }
 
-        #endregion
+//        #endregion
 
-        private void PostRequestHandlerExecute(HttpApplication app)
-        {
-            string contentType = app.Response.ContentType;
-            string method = app.Request.HttpMethod;
-            int status = app.Response.StatusCode;
-            IHttpHandler handler = app.Context.CurrentHandler;
+//        private void PostRequestHandlerExecute(HttpApplication app)
+//        {
+//            string contentType = app.Response.ContentType;
+//            string method = app.Request.HttpMethod;
+//            int status = app.Response.StatusCode;
+//            IHttpHandler handler = app.Context.CurrentHandler;
 
-            if (contentType == "text/html" && method == "GET" && status == 200 && handler != null)
-            {
-                app.Response.Filter = new WhitespaceFilter(app.Response.Filter, app.Request.ContentEncoding);
-            }
-        }
+//            if (contentType == "text/html" && method == "GET" && status == 200 && handler != null)
+//            {
+//                app.Response.Filter = new WhitespaceFilter(app.Response.Filter, app.Request.ContentEncoding);
+//            }
+//        }
 
-        #region Stream filter
+//        #region Stream filter
 
-        private class WhitespaceFilter : Stream
-        {
-            private readonly Encoding _encoding;
-            private readonly Stream _stream;
-            private readonly MemoryStream _cache;
-            private readonly static HtmlMinifier _minifier = new HtmlMinifier(new HtmlMinificationSettings
-            {
-                WhitespaceMinificationMode = WhitespaceMinificationMode.Aggressive,
-                RemoveRedundantAttributes = false
-            });
+//        private class WhitespaceFilter : Stream
+//        {
+//            private readonly Encoding _encoding;
+//            private readonly Stream _stream;
+//            private readonly MemoryStream _cache;
+//            private readonly static HtmlMinifier _minifier = new HtmlMinifier(new HtmlMinificationSettings
+//            {
+//                WhitespaceMinificationMode = WhitespaceMinificationMode.Aggressive,
+//                RemoveRedundantAttributes = false
+//            });
 
-            public WhitespaceFilter(Stream sink, Encoding encoding)
-            {
-                _stream = sink;
-                _encoding = encoding;
-                _cache = new MemoryStream();
-            }
+//            public WhitespaceFilter(Stream sink, Encoding encoding)
+//            {
+//                _stream = sink;
+//                _encoding = encoding;
+//                _cache = new MemoryStream();
+//            }
 
-            #region Properites
+//            #region Properites
 
-            public override bool CanRead
-            {
-                get { return true; }
-            }
+//            public override bool CanRead
+//            {
+//                get { return true; }
+//            }
 
-            public override bool CanSeek
-            {
-                get { return true; }
-            }
+//            public override bool CanSeek
+//            {
+//                get { return true; }
+//            }
 
-            public override bool CanWrite
-            {
-                get { return true; }
-            }
+//            public override bool CanWrite
+//            {
+//                get { return true; }
+//            }
 
-            public override void Flush()
-            {
-                _stream.Flush();
-            }
+//            public override void Flush()
+//            {
+//                _stream.Flush();
+//            }
 
-            public override long Length
-            {
-                get { return 0; }
-            }
+//            public override long Length
+//            {
+//                get { return 0; }
+//            }
 
-            private long _position;
-            public override long Position
-            {
-                get { return _position; }
-                set { _position = value; }
-            }
+//            private long _position;
+//            public override long Position
+//            {
+//                get { return _position; }
+//                set { _position = value; }
+//            }
 
-            #endregion
+//            #endregion
 
-            #region Methods
+//            #region Methods
 
-            public override int Read(byte[] buffer, int offset, int count)
-            {
-                return _stream.Read(buffer, offset, count);
-            }
+//            public override int Read(byte[] buffer, int offset, int count)
+//            {
+//                return _stream.Read(buffer, offset, count);
+//            }
 
-            public override long Seek(long offset, SeekOrigin origin)
-            {
-                return _stream.Seek(offset, origin);
-            }
+//            public override long Seek(long offset, SeekOrigin origin)
+//            {
+//                return _stream.Seek(offset, origin);
+//            }
 
-            public override void SetLength(long value)
-            {
-                _stream.SetLength(value);
-            }
+//            public override void SetLength(long value)
+//            {
+//                _stream.SetLength(value);
+//            }
 
-            public override void Write(byte[] buffer, int offset, int count)
-            {
-                _cache.Write(buffer, offset, count);
-            }
+//            public override void Write(byte[] buffer, int offset, int count)
+//            {
+//                _cache.Write(buffer, offset, count);
+//            }
 
-            public override void Close()
-            {
-                byte[] buffer = _cache.ToArray();
-                int cacheSize = buffer.Length;
+//            public override void Close()
+//            {
+//                byte[] buffer = _cache.ToArray();
+//                int cacheSize = buffer.Length;
 
-                string original = _encoding.GetString(buffer);
-                string result = _minifier.Minify(original).MinifiedContent;
-                byte[] output = _encoding.GetBytes(result);
+//                string original = _encoding.GetString(buffer);
+//                string result = _minifier.Minify(original).MinifiedContent;
+//                byte[] output = _encoding.GetBytes(result);
 
-                _stream.Write(output, 0, output.Length);
-                _cache.Dispose();
-                _stream.Dispose();
-            }
+//                _stream.Write(output, 0, output.Length);
+//                _cache.Dispose();
+//                _stream.Dispose();
+//            }
 
-            #endregion
+//            #endregion
 
-        }
+//        }
 
-        #endregion
+//        #endregion
 
-    }
-}
+//    }
+//}
